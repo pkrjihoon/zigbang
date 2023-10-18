8bfb3e2fcd212c21d87a70057c8dda8e2ee73f8b
박지훈 <roal145@naver.com>
2023-10-18 21:31:57 +0900

    test222

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          지훈짱짱
-          선희짱짱짱
-          박지훈짱짱짱
-        </p>
+        <Bure>
+          <p>지훈짱짱</p>
+          <p>선희짱짱짱</p>
+          <p>박지훈짱짱짱</p>
+          <p>박지훈짱짱짱</p>
+        </Bure>
         <p>
           빠빠빠빠빠!!!!1
           빠빠빠빠빠!!!!2
           빠빠빠빠빠!!!!122222
         </p>
+         박지훈짱
         {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -29,4 +34,9 @@ function App() {
   );
 }
 
+const Bure = styled.div`
+  color: red;
+  font-size: 40px;
+  text-align: center;
+`;
 export default App;
